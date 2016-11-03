777b065c1c89e3e090301d9a50a7f9d1e36bd726
Krishna Teja M <teja.neotech@gmail.com>
2016-11-03 09:09:34 +0530

    First Commit
    Changed one import statement ;)

--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/addedittask/AddEditTaskFragment.java
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/addedittask/AddEditTaskFragment.java
@@ -22,11 +22,11 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.support.v4.app.*;
 
 import com.example.android.architecture.blueprints.todoapp.R;
 
